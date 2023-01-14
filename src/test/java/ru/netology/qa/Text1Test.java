524a8026bbf1d259c9245a026e1d53e5016dffc4
Надежда Берестова <berestanadi@gmail.com>
2023-01-14 16:13:03 +0300

    Fix Test

--- src/test/java/ru/netology/qa/Text1Test.java
+++ src/test/java/ru/netology/qa/Text1Test.java
@@ -1,40 +1,32 @@
 package ru.netology.qa;
 
-import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.netology.qa.screens.MainScreen;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
-import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
-import static io.appium.java_client.remote.MobileCapabilityType.DEVICE_NAME;
 import static org.junit.jupiter.api.TestInstance.Lifecycle;
-import static org.openqa.selenium.remote.CapabilityType.PLATFORM_NAME;
 
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class Text1Test {
 
-    private AppiumDriver driver;
+    private AndroidDriver driver;
 
     @BeforeAll
     public void createDriver() throws MalformedURLException {
-        String platform = System.getProperty("platform");
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        if ("android".equals(platform)) {
-            desiredCapabilities.setCapability(PLATFORM_NAME, "android");
-            desiredCapabilities.setCapability(DEVICE_NAME, "someName");
-            desiredCapabilities.setCapability(APP_PACKAGE, "ru.netology.testing.uiautomator");
-            desiredCapabilities.setCapability(APP_ACTIVITY, "ru.netology.testing.uiautomator.MainActivity");
-        } else {
-            throw new IllegalArgumentException(String.format("Platform %s no supported", platform));
-        }
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("diveceName", "someName");
+        desiredCapabilities.setCapability("appPackage", "ru.netology.testing.uiautomator");
+        desiredCapabilities.setCapability("appActivity", "ru.netology.testing.uiautomator.MainActivity");
 
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        URL remoteURL = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteURL,desiredCapabilities);
     }
 
     @Test
@@ -66,8 +58,7 @@ public class Text1Test {
 
     @AfterAll
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+        driver.quit();
         }
     }
-}
+
